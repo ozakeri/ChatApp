5b6d671b2eee58b50ef9b6d2b28996c4542c53b5
Omid Zakeri <omidzakeri@gmail.com>
2022-01-08 17:40:58 +0330

    commit by omid

---
+++ app/src/main/java/com/gap/chat_app/util/StartCamera.java
@@ -0,0 +1,5 @@
+package com.gap.chat_app.util;
+
+public class StartCamera {
+
+}
---
+++ app/src/main/java/com/gap/chat_app/webservice/ServiceApi.java
@@ -0,0 +1,9 @@
+package com.gap.chat_app.webservice;
+
+public class ServiceApi {
+
+	public static String ServerApi = "http://www.test.com/";
+	public static String EducationApi = ServerApi+"api/education.json";//amuzeshgah
+	public static String LoginApi = ServerApi+"api/user/login.json";//login username and password
+
+}
